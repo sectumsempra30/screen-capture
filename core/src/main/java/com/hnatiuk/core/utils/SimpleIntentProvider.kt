23bf2bb72edf8c6408fece5b35e1a83863398486
maximhnatiuk <maxim.hnatiuk@gmail.com>
2022-11-27 00:07:29 +0200

    Add new module with other solutions Add sample for edit text focus losing when touch outside

--- core/src/main/java/com/hnatiuk/core/utils/SimpleIntentProvider.kt
+++ core/src/main/java/com/hnatiuk/core/utils/SimpleIntentProvider.kt
@@ -3,7 +3,7 @@ package com.hnatiuk.core.utils
 import android.content.Context
 import android.content.Intent
 
-abstract class SimpleIntentProvider(private val clazz: Class<*>){
+abstract class SimpleIntentProvider(private val clazz: Class<*>) {
 
     fun getIntent(context: Context): Intent {
         return Intent(context, clazz)
---
+++ other/src/main/java/dev/hnatiuk/other/FragmentEditTextLostFocusOutside.kt
@@ -0,0 +1,42 @@
+package dev.hnatiuk.other;
+
+import android.annotation.SuppressLint
+import android.graphics.Rect
+import android.view.MotionEvent
+import android.widget.EditText
+import com.hnatiuk.core.base.BaseFragment
+import com.hnatiuk.core.base.Inflate
+import com.hnatiuk.core.utils.hideKeyboard
+import dev.hnatiuk.other.databinding.FragmentEditTextLostFocusOutsideBinding
+
+class FragmentEditTextLostFocusOutside : BaseFragment<FragmentEditTextLostFocusOutsideBinding>() {
+
+    override val bindingFactory: Inflate<FragmentEditTextLostFocusOutsideBinding>
+        get() = FragmentEditTextLostFocusOutsideBinding::inflate
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun FragmentEditTextLostFocusOutsideBinding.initUI() {
+        root.setOnTouchListener { _, motionEvent ->
+            onDispatchEvent(motionEvent, binding.editText)
+            false
+        }
+    }
+
+    private fun onDispatchEvent(event: MotionEvent, editText: EditText) {
+        if (event.action == MotionEvent.ACTION_DOWN && editText.isFocused) {
+            val editTextRect = Rect().apply { editText.getGlobalVisibleRect(this) }
+            val textViewOutRect = Rect().apply { binding.text1.getGlobalVisibleRect(this) }
+
+            val (rawX, rawY) = event.rawX.toInt() to event.rawY.toInt()
+            if (!editTextRect.contains(rawX, rawY) && !textViewOutRect.contains(rawX, rawY)) {
+                editText.clearFocus()
+                requireContext().hideKeyboard(editText)
+            }
+        }
+    }
+
+    companion object {
+
+        fun newInstance() = FragmentEditTextLostFocusOutside()
+    }
+}
