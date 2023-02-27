042d8fe4051ff9ab0081be166884ca00cf31cd94
maximhnatiuk <maxim.hnatiuk@gmail.com>
2023-02-27 16:28:20 +0200

    Compose: StartAndroid - 3 (Modifiers) Lessons

--- compose/src/main/java/dev/hnatiuk/android/samples/course/HomeScreen.kt
+++ compose/src/main/java/dev/hnatiuk/android/samples/course/HomeScreen.kt
@@ -1,28 +1,45 @@
 package dev.hnatiuk.android.samples.course
 
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(userName: String) {
-    Text(
-        text = "User: $userName",
-        fontSize = 20.sp,
-        color = Color.Cyan
-    )
+    Box {
+        Text(
+            text = "User: $userName",
+            fontSize = 20.sp,
+            color = Color.Cyan,
+            textAlign = TextAlign.End,
+            modifier = Modifier
+                .background(
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(size = 10.dp)
+                )
+                .width(200.dp)
+                .height(100.dp)
+        )
+    }
 }
 
 @Preview(
-    name = "Custom Preview",
+    widthDp = 400,
+    heightDp = 800,
     showBackground = true,
-    showSystemUi = true,
-    uiMode = UI_MODE_TYPE_NORMAL,
-    device = Devices.PIXEL_4
+    showSystemUi = false,
+    uiMode = UI_MODE_TYPE_NORMAL
 )
 @Composable
 fun HomeScreenPreview() {
