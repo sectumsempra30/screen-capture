f99caf4ab56e899b2b250a8ec8287111ea1e0baa
maximhnatiuk <maxim.hnatiuk@gmail.com>
2023-03-02 00:35:32 +0200

    Compose: StartAndroid - 4 (Layouts) Lessons

--- compose/src/main/java/dev/hnatiuk/android/samples/course/HomeScreen.kt
+++ compose/src/main/java/dev/hnatiuk/android/samples/course/HomeScreen.kt
@@ -2,22 +2,35 @@ package dev.hnatiuk.android.samples.course
 
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(userName: String) {
-    Box {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "First text in layout",
+            modifier = Modifier
+                .background(color = Color.LightGray)
+
+        )
         Text(
             text = "User: $userName",
             fontSize = 20.sp,
@@ -28,8 +41,6 @@ fun HomeScreen(userName: String) {
                     color = Color.Gray,
                     shape = RoundedCornerShape(size = 10.dp)
                 )
-                .width(200.dp)
-                .height(100.dp)
         )
     }
 }
